b25f66415cd6a69995571f66866e72c524bc5310
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2022-11-05 22:11:50 +0200

    Update

---
+++ On_demand_Traffic_light_control_SW/main.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+ *
+ * File Name   : main.c
+ *
+ * Description : Source file for the main function
+ *
+ * Author      : Abdelrahman Hesham
+ *
+ *******************************************************************************/
+
+#include "APP/app.h"
+#include "avr/io.h"
+#include "MCAL/EXT_INT/ext_int.h"
+#include "HAL/LED/led.h"
+
+
+#if TEST_MODE==1
+#include <avr/interrupt.h>
+#endif
+
+
+int main(void){
+
+	INT0_Init();				/*to initialize external interrupt*/
+	Led_Init();					/*to initialize Leds*/
+	SREG|=(1<<7);               /*Enabling Global interrupt*/
+
+
+#if TEST_MODE!=1
+	app_start();				/*start of the application*/
+#endif
+
+#if TEST_MODE==1
+	while(1);
+#endif
+
+
+}
